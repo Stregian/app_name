477c3c552fc567577dce209b2cf17a0d3f92b93d
Strix <stregian@gmail.com>
2017-01-31 08:15:02 +0000

    made projects index view a list, added template

--- app_name/projects/views.py
+++ app_name/projects/views.py
@@ -2,11 +2,12 @@
 from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse
 from .models import Project
+
 # Create your views here.
 def index(request):
-	html = 'index for the projects page'
-	return HttpResponse(html)
-	
+    project_list = Project.objects.order_by('slug')
+    context ={'project_list': project_list} 
+    return render(request, 'projects/index.html', context)
 	
 def project(request, slug):
 	p = get_object_or_404(Project, slug=slug)
